a019f26b01dcc88364816b900ef51b66d11e042f
lavens <lavens2014@gmail.com>
2020-11-11 23:34:13 +0300

    Второй урок

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const namberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 const personalMovieDB = {
     count: namberOfFilms,
@@ -7,13 +9,27 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('один из просмотреных фильмов?', ''),
-      b = prompt('На сколько оцените его?',''),
-      c = prompt('Один из просмотренных фильмов?',''),
-      d = prompt('На сколько оцените его?','');
+for (let i = 0; i < 2; i++) {
+    const a = prompt('один из просмотреных фильмов?', ''),
+          b = prompt('На сколько оцените его?','');
 
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done')
+    } else {
+        console.log('error');
+        i--;
+    }
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+if (personalMovieDB.count < 10){
+    console.log("Просмотренно давольно маловато фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log ("Вы киноман");   
+} else {
+    console.log("Произошла ошибка");
+}
 
 console.log(personalMovieDB);
